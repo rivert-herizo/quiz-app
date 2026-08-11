6e31e9d16d0c40085d353549a47052eac7d29c3a
neko <riverherizo@proton.me>
2026-08-11 09:53:58 +0300

    add question

---
+++ questions.js
@@ -0,0 +1,14 @@
+export const questions = [
+  {
+    question: "What is the biggest island in the Indian Ocean ?",
+    choices: ["La Reunion", "Mauritius", "Madagascar"],
+  },
+  {
+    question: "What is the biggest lemur species in Madagascar ?",
+    choices: ["Indri", "Microsebus", "Propithecus"],
+  },
+  {
+    question: "How many baobab is endemic to Madagascar ?",
+    choices: ["2", "1", "6"],
+  },
+];
